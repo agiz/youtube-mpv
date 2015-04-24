3ee3133f20299b1cadd34eaceed73583c6dd2f0a
noface <noface@outlook.com>
2015-04-24 16:28:48 +0200

    update trigger

--- firefox/data/trigger.js
+++ firefox/data/trigger.js
@@ -1,7 +1,7 @@
 self.on('context', function(node) {
   if (node.href) {
     self.on('click', function(node, data) {
-      return self.postMessage(node.href);
+      return self.postMessage([node.href, Date.now()]);
     });
     return true;
   }
