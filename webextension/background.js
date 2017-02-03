6a7b792f3831cca7bc0a48e7be8618a37586e54f
Jamie Tanna <jamie@jamietanna.co.uk>
2017-02-03 11:09:11 +0000

    Add ability to play a link, not just current page

--- webextension/background.js
+++ webextension/background.js
@@ -2,27 +2,41 @@
 Create all the context menu items.
 */
 browser.contextMenus.create({
-  id: "play-with-mpv",
-  title: browser.i18n.getMessage("contextMenuItemPlayWithMpv"),
-  contexts: ["all"]
+  id: "play-page-with-mpv",
+  title: browser.i18n.getMessage("contextMenuItemPlayPageWithMpv"),
+  contexts: ["page"]
+});
+
+browser.contextMenus.create({
+  id: "play-link-with-mpv",
+  title: browser.i18n.getMessage("contextMenuItemPlayLinkWithMpv"),
+  contexts: ["link"]
 });
 
 browser.contextMenus.onClicked.addListener(function(info, tab) {
+  var urlToPlay = null;
   switch (info.menuItemId) {
-    case "play-with-mpv":
-      var req = new XMLHttpRequest();
-      if(req) {
-        req.open('GET', 'http://localhost:9000/p?i='+tab.url, true);
-        req.onreadystatechange =  function() {
-          if (req.readyState == 4) {
-            console.log("Response: " + req.responseText);
-          }
-        };
-        req.send();
-      }
+    case "play-page-with-mpv":
+      urlToPlay = tab.url;
+      break;
+    case "play-link-with-mpv":
+      urlToPlay = info.linkUrl;
       break;
     default:
       console.log("Unknown context menu option: " + info.menuItemId);
       break;
+  }
+
+  if (urlToPlay) {
+    var req = new XMLHttpRequest();
+    if(req) {
+      req.open('GET', 'http://localhost:9000/p?i='+urlToPlay, true);
+      req.onreadystatechange =  function() {
+        if (req.readyState == 4) {
+          console.log("Response: " + req.responseText);
+        }
+      };
+      req.send();
+    }
   }
 });
